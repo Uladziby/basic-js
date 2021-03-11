cf212e98b8eb6863b95719adb6646a98fedf6a3f
Wlad <yanushevskyv@gmail.com>
2021-03-11 22:54:00 +0300

    few a tasks

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
+module.exports = function countCats( matrix ) {
+  this.matrix = matrix;
+  let countCat = 0;
+  let i;
+  let j;
+  for(i=0; i< matrix.length; i++){
+    for(j=0; j<matrix[i].length;j++){
+      if (matrix[i][j]==='^^'){
+        countCat++;
+      }
+    }
+  }
+  return countCat;
   // remove line with error and write your code here
 };
--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function getSeason(date) {
+if (!date) return "Unable to determine the time of year!";
+if (!date.getTime()) return "Error";
+const str = date.getMonth()
+if (str === 11 || str === 0 || str === 1) {
+  return "winter";
+  } else if ((str >= 2 && str <= 4)) {
+    return "spring";
+  } else if ((str >= 5 && str <= 7)) {
+    return "summer";
+  } else if ((str >= 8 && str <= 10)) {
+    return "autumn";
+} else throw "Error";
+
+}
